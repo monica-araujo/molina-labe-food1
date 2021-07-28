5fd6d6f3c02786338428d223257039849d5164cd
Robinndev <robson_dev@hotmail.com>
2021-07-27 23:58:40 -0300

    finalizando pagina de busca

---
+++ rappi4/src/components/barraBusca/SearchBar.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import { IconButton, Paper, InputBase, BottomNavigation, BottomNavigationAction } from '@material-ui/core'
+import SearchIcon from '@material-ui/icons/Search';
+import { MainContainer} from './Styled'
+
+export const SearchBar = () => {
+    return(
+        <div>
+             <MainContainer>
+                       <Paper 
+                    component="form" 
+                    variant={"outlined"}
+                    style={{ width: "90vw" }}
+
+                >
+                    <IconButton  >
+                        <SearchIcon 
+                            color={'tertiary'}
+                        />
+                    </IconButton>
+                    <InputBase
+                        placeholder="Restaurante"
+                    />          
+                </Paper>
+                </MainContainer>
+        </div>
+    )
+}
